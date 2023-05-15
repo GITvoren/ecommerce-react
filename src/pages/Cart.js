bbbc1a2837cebd955a15982772aa9af1894d2967
GITvoren <tejuco.voren@gmail.com>
2023-05-16 00:56:06 +0800

    added notification

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,14 +2,25 @@ import '../assets/css/cart.css'
 import CartContext from '../utilities/CartContext.js'
 import {useContext, useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom'
-
 import CartItem from '../components/CartItem.js'
+import {toast, Slide} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 function Cart(){
      const navigate = useNavigate();
      const {cartItems, setCart} = useContext(CartContext);
+     const notify = () => toast.info('Currently making a new and better Ecommerce API. Coming soon 🙏', {
+          position: "top-center",
+          autoClose: false,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
 
      const totalPrice = cartItems.reduce((price, item) => price + (item.quantity * item.price), 0)
 
@@ -53,7 +64,7 @@ function Cart(){
                                    <div className="cart-bottom">
                                         <h3>Total: <span>&#8369;{totalPrice}</span></h3>
                                         <div className="checkout-btn-cart">
-                                             <button>CHECKOUT</button>
+                                             <button onClick={()=> notify()}>CHECKOUT</button>
                                         </div>
                                    </div>    
                          </div>
